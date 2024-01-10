de76e6afbbb61d93144d79ca9223395ad4f8e9d2
lonewolf <bajpaiutkarsh1104@gmail.com>
2024-01-10 10:03:48 +0530

    list page init

--- app.js
+++ app.js
@@ -8,9 +8,15 @@ app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('home')
 })
 
+app.get('/list', (req, res) => {
+    res.render('list')
+})
+app.post('/', (req, res) => {
+    res.redirect('/list')
+})
 app.listen(1111, () => {
     console.log("Server running on http://localhost:1111");
 })
