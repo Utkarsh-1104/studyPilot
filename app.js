d23803742fe16496cf89db7fb4b0455323313ad4
lonewolf <bajpaiutkarsh1104@gmail.com>
2024-01-12 16:02:24 +0530

    .

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ app.use(express.static('public'))
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: true }))
 
+const goals = []
+
 app.get('/', (req, res) => {
     res.render('home')
 })
 
 app.get('/list', (req, res) => {
-    res.render('list')
+    res.render('list', { goalsList: goals })
 })
 
 app.get('/compose', (req, res) => {
@@ -24,8 +26,10 @@ app.post('/', (req, res) => {
 })
 
 app.post('/compose', (req, res) => {
+    goals.push(req.body.goal)
     res.redirect('/list')
 })
+
 app.listen(1111, () => {
     console.log("Server running on http://localhost:1111");
 })
